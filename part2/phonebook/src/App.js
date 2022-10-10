de0986f5c4b20693a4e62c27075fe515d210ea23
Roman <romandescotte@gmail.com>
2022-10-11 12:38:24 +1300

    exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   console.log('inicio app()')
   const [persons, setPersons] = useState([]) 
 
-  const hook = () => {
+  const getData = () => {
     const url='http://localhost:3001/persons/'  
     axios 
       .get(url)
@@ -19,7 +19,7 @@ const App = () => {
         setPersons(response.data)
       })
   }
-  useEffect(hook, [])
+  useEffect(getData, [])
 
   const [newEntry, setNewEntry] = useState({name: '', number: ''}) 
   const [search, setSearch] = useState('')
@@ -38,19 +38,28 @@ const App = () => {
     }); 
   }  
 
-  const handleSaveEntry = (event) => {
+  const createData = (event) => {
     event.preventDefault()
     const newPerson = {      
       name: newEntry.name,
       number: newEntry.number
     }
-
+    
     if(!persons.map(person => person.name).includes(newEntry.name)) {
-      setPersons(persons.concat(newPerson))
-      setNewEntry({
-        name: '', 
-        number: ''
+      axios
+      .post('http://localhost:3001/persons/', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data));
+        setNewEntry({
+          name: '', 
+          number: ''
+        });
       })
+      .catch(error => {
+        console.error(`HTTP POST Failed: ${error}`)
+      })
+
+      
     } else {
       alert(`${newEntry.name} is already added to phonebook`)
     }    
@@ -70,7 +79,7 @@ const App = () => {
       <h1>Phonebook</h1>      
       filter shown with: <Search onChange={handleSearch} value={search} />
       <h2>Add a new</h2>
-      <PersonForm handleNewName={handleNewName} handleNewNumber={handleNewNumber} onSubmit={handleSaveEntry} number={newEntry.number} name={newEntry.name} />
+      <PersonForm handleNewName={handleNewName} handleNewNumber={handleNewNumber} onSubmit={createData} number={newEntry.number} name={newEntry.name} />
       <h2>Numbers</h2>      
       <Persons persons={filteredEntries} /> 
     </div>
