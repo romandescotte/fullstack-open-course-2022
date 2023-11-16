fb2cf25fb1ec333e9895e5edbcce8b4ce81b5431
Roman <romandescotte@gmail.com>
2023-11-16 22:25:50 +1300

    exercise 4.2

---
+++ part4/utils/config.js
@@ -0,0 +1,7 @@
+const PORT = 3003
+const MONGODB_URI = 'mongodb+srv://rouman:2wNLm0Ie7u3ib9Oe@cluster0.w7ocrxy.mongodb.net/blogList?retryWrites=true&w=majority'
+
+module.exports = { 
+  PORT,
+  MONGODB_URI 
+}
