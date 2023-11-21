b9fca47d27a2f3864a036ecc3415ebf11ec7353a
Roman <romandescotte@gmail.com>
2023-11-21 21:48:34 +1300

    exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,8 +1,16 @@
-
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  
+  return blogs.length === 1 
+  ? blogs[0].likes 
+  : blogs.reduce((sum, blog) => sum + blog.likes,0)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
