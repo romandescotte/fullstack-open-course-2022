16bd7eb6b4b57dac0d462489323b1f35a75b0185
Roman <romandescotte@gmail.com>
2023-11-22 12:38:28 +1300

    exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,7 @@ const dummy = (blogs) => {
   return 1
 }
 
+
 const totalLikes = (blogs) => {
   
   return blogs.length === 1 
@@ -9,8 +10,23 @@ const totalLikes = (blogs) => {
   : blogs.reduce((sum, blog) => sum + blog.likes,0)
 }
 
+const favoriteBlog = (blogs) => {  
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes)) 
+  return blogs.length >= 1
+  ? blogs.find(blog => blog.likes === maxLikes)  
+  : undefined
+}
+
+/*
+otra forma de hacerlo con reduce:
+const maxLikes = blogs.reduce((accumulator, current) => {
+  return accumulator.id > current.id ? accumulator : current;
+});
+*/ 
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
